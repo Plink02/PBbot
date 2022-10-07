309daa93e683db2f53f8aa0c28d91663b6219fd1
Plink02 <88978703+Plink02@users.noreply.github.com>
2022-10-07 19:02:50 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,67 @@
+import discord
+from twitchio.ext import commands
+import os
+import asyncio
+from dotenv import load_dotenv
+from srcapi import findPB
+
+load_dotenv()
+
+Token = os.environ.get('DISCORD_BOT_SECRET')
+client = discord.Client()
+twitchBot = commands.Bot(
+  token = os.environ['TWITCH_TOKEN'],
+  client_id = os.environ['TWITCH_CLIENT_ID'],
+  nick='pacealertbot',
+  prefix='!',
+  initial_channels=['plink02']
+)
+
+
+async def send_alert(alert, ctx):
+  try:
+    channels = client.get_all_channels()
+    channel = discord.utils.get(channels, name="tests")
+    guild = channel.guild
+    role = discord.utils.get(guild.roles, name="Pace Alerts")
+    await channel.send(f"""
+    {role.mention}
+    {alert}
+    """)
+    await ctx.send("Pace alert sent")
+  except:
+    await ctx.send("Failed to send pace alert")
+
+  
+@twitchBot.command(name='alert')
+async def pace_alert_command(ctx):
+  info = ctx.message.content
+  info = info[6::]
+  user = ctx.channel.name
+  twitchUrl = " https://www.twitch.tv/" + user
+  alert = user + info + twitchUrl
+  print(alert)
+  await send_alert(alert, ctx)
+
+@twitchBot.command(name='pb')
+async def get_player_pb(ctx):
+  content = ctx.message.content
+  content = content[4::]
+  params = content.split(None,1)
+  player = params[0]
+  #if only player is entered, default to any%/single segment
+  if len(params) > 1:
+    category = params[1]
+    pb = findPB(player, category)
+  else:
+    pb = findPB(player)
+  await ctx.send(pb)
+
+
+
+if __name__ == '__main__':
+  loop = asyncio.get_event_loop()
+  loop.create_task(client.start(Token))
+  loop.create_task(twitchBot.run())
+  loop.run_forever()
+  
---
+++ srcapi.py
@@ -0,0 +1,54 @@
+from asyncio.windows_events import NULL
+import srcomapi, srcomapi.datatypes as dt
+import datetime
+
+api = srcomapi.SpeedrunCom(); api.debug = 1
+
+
+
+def findPB(player, categoryName="Single Segment"):
+    #search and obtain game
+    games = api.search(dt.Game, {"name": "Mike Tyson"})
+    game = games[0]
+
+    category = NULL
+    level = NULL
+
+    #find user, if user not found return sorry message
+    try:
+        users = api.search(dt.User, {"name": player})
+        userId = users[0].id
+    except:
+        return("Sorry! I couldn't find a PB for that user.")
+
+    #find category/level, if not found, send sorry message
+    try:
+        for cat in game.categories:
+            if cat.name.lower() == categoryName.lower():
+                category = cat
+        if category == NULL:
+            category = game.categories[1]
+            for lev in game.levels:
+                if lev.name.lower() == categoryName.lower():
+                    level = lev
+    except:
+        return("Sorry! I couldn't locate that run category.")
+
+    #if level is null find category leaderboard, otherwise find the specific level board
+    if level == NULL:
+        leaderboard = dt.Leaderboard(api, data=api.get("leaderboards/{}/category/{}".format(game.id, category.id)))
+    else:
+        leaderboard = dt.Leaderboard(api, data=api.get("leaderboards/{}/level/{}/{}".format(game.id, level.id, category.id)))
+    #local version of leaderboard created to prevent new API calls for each run
+    localBoard = leaderboard.data
+
+    for run in localBoard["runs"]:
+        if run["run"]["players"][0]["rel"] == "user" and run["run"]["players"][0]["id"] == userId:
+           time = run["run"]["times"]["primary_t"]
+           time = datetime.timedelta(seconds=time)
+           time = str(time)
+           time = time[2:10]
+           message = player + "'s " + categoryName + " PB is " + time
+           return(message)
+    #if nothing found, user does not have submitted run in category, return sorry message
+    return("Sorry! That player hasn't submitted a PB for that.")
